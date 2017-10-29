d9d26939260768c62f426303a5cde08555c80b94
Nilaan <g.nilaan@gmail.com>
2017-10-29 14:07:41 -0400

    Removed build files

--- build/lab2/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jalaj/catkin_ws/src/lab2
-# Build directory: /home/jalaj/catkin_ws/build/lab2
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/turtlebot_example/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jalaj/catkin_ws/src/turtlebot_example
-# Build directory: /home/jalaj/catkin_ws/build/turtlebot_example
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
